3e1c9a00da219c7d5f6c6cafc6030cc7d94d290d
Joe Tustin <j.tustin@gmail.com>
2019-02-27 21:52:47 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -9,9 +9,9 @@ function lastImageSelector() {
 }
 
 function ninjaBabySelector() {
-  return $("#baby-ninja")
+  return $("#baby-ninja");
 }
 
 function divSelector() {
-  return $(".pics")
+  return $(".pics");
 }
